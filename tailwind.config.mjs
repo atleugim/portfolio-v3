473c72102e7491860f46abfec39b4734e8611452
Miguel Vega <vegaleugim@gmail.com>
2024-08-26 09:28:08 -0500

    improved colors with tailwind and variables

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,14 @@ export default {
   darkMode: ['class'],
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        background: 'rgb(var(--background) / <alpha-value>)',
+        foreground: 'rgb(var(--foreground) / <alpha-value>)',
+        dimmed: 'rgb(var(--dimmed) / <alpha-value>)',
+        card: 'rgb(var(--border) / <alpha-value>)'
+      }
+    }
   },
   plugins: [require('tailwindcss-bg-patterns')]
 };
