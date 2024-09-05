d7756440e344aa132365d144344f65ee6448f580
Miguel Vega <vegaleugim@gmail.com>
2024-09-05 00:10:24 -0500

    fix some translations

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -10,12 +10,16 @@ export const ui = {
   en: {
     'nav.home': 'Home',
     'nav.projects': 'Projects',
+    'contact.email': 'Email',
+    'contact.download_cv': 'Download CV',
     'footer.spotify.title': 'Now playing...',
     'footer.spotify.not-playing': 'Nothing 😴'
   },
   es: {
     'nav.home': 'Inicio',
     'nav.projects': 'Proyectos',
+    'contact.email': 'Correo electrónico',
+    'contact.download_cv': 'Descargar CV',
     'footer.spotify.title': 'Escuchando...',
     'footer.spotify.not-playing': 'Nada 😴'
   }
